6c06cf2d6428a7fea2d1b23c713f9a30071bb0dd
Eckorezze <alexandrsoloby@gmail.com>
2024-05-12 18:21:57 +0300

    metrics

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,28 @@
 #include "handlers/rates/handler.hpp"
 #include "handlers/update/handler.hpp"
 #include "hello.hpp"
+#include "userver/alerts/handler.hpp"
 #include "userver/clients/dns/component.hpp"
+#include "userver/components/common_component_list.hpp"
+#include "userver/components/common_server_component_list.hpp"
 #include "userver/storages/postgres/component.hpp"
 #include "userver/storages/secdist/component.hpp"
 #include "userver/storages/secdist/provider_component.hpp"
 
 int main(int argc, char* argv[]) {
   auto component_list =
-      userver::components::MinimalServerComponentList()
+      userver::components::ComponentList()
+          .AppendComponentList(userver::components::CommonComponentList())
+          .AppendComponentList(userver::components::CommonServerComponentList())
           .Append<userver::server::handlers::Ping>()
-          .Append<userver::components::TestsuiteSupport>()
-          .Append<userver::components::HttpClient>()
-          .Append<userver::server::handlers::TestsControl>()
           .Append<components::rate_manager::Component>()
           .Append<userver::components::Secdist>()
           .Append<userver::components::DefaultSecdistProvider>()
           .Append<handlers::currencies::Handler>()
           .Append<handlers::rates::Handler>()
           .Append<handlers::update::Handler>()
-          .Append<userver::components::Postgres>("postgres-db-1")
-          .Append<userver::clients::dns::Component>();
+          .Append<userver::alerts::Handler>()
+          .Append<userver::components::Postgres>("postgres-db-1");
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
